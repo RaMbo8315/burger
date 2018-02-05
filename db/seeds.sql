3eb54edfa8ceddbff2e8e83ed9cb6a238552c056
RaMbo8315 <henryurena@yahoo.com>
2018-02-04 21:49:32 -0500

    Created directorys and files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Avocado Angus Burger", false );
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mozzerella Steak Burger", false );
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Slam Burger", false );
